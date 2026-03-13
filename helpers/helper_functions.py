06cb05bd3092a5149310e380843783fd25533c83
Daniel Dawson Jensen <dadj@aarhus.dk>
2026-03-13 13:18:54 +0100

    updated to v001

---
+++ helpers/helper_functions.py
@@ -0,0 +1,71 @@
+"""Helper functions"""
+
+import os
+import logging
+
+from datetime import date
+
+from dotenv import load_dotenv
+
+from mbu_process_dashboard_shared_components.process_dashboard_client import ProcessDashboardClient
+from mbu_process_dashboard_shared_components import (
+    process_run,
+    process_step_run,
+)
+
+from mbu_solteqtand_shared_components.database.db_handler import SolteqTandDatabase
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+API_ADMIN_TOKEN = os.getenv("API_ADMIN_TOKEN")
+CLIENT = ProcessDashboardClient(api_admin_token=API_ADMIN_TOKEN)
+
+
+def handle_dashboard_run_creation(process_name: str, meta: dict):
+    """
+    Method for handling the creation of new process dashboard runs - if run already exists for the citizen, no new process run is created
+    """
+
+    print(f"meta: {meta}")
+
+    citizen_cpr = meta.get("cpr")
+
+    existing_run_id = process_run.get_process_run_by_cpr(client=CLIENT, process_name=process_name, cpr=citizen_cpr)
+
+    if existing_run_id:
+        logger.info("Process run already exists for citizen")
+
+    else:
+        process_run.create_dashboard_run(client=CLIENT, process_name=process_name, meta=meta)
+
+
+def handle_process_dashboard(status: str, item_reference: str, process_step_name: str, failure: Exception | None = None, process_name: str = "Frit valg"):
+    """
+    Method for handling updating the process dashboard
+    """
+
+    status_update_data = {
+        "status": status
+    }
+
+    citizen_cpr = item_reference
+
+    logger.info("before get_step_run_id_for_process_step_cpr() ...")
+
+    step_run_id = process_step_run.get_step_run_id_for_process_step_cpr(client=CLIENT, process_name=process_name, step_name=process_step_name, cpr=citizen_cpr)
+
+    if failure:
+        step_run_update_data = process_step_run.build_step_run_update(status=status, failure=failure)
+
+        status_update_data["failure"] = failure
+
+    else:
+        step_run_update_data = process_step_run.build_step_run_update(status=status)
+
+    logger.info("before update_dashboard_step_run_by_id() ...")
+
+    updated_step_run_data, status_code = process_step_run.update_dashboard_step_run_by_id(client=CLIENT, step_run_id=step_run_id, update_data=step_run_update_data)
+
+    return updated_step_run_data, status_code
--- processes/process_item.py
+++ processes/process_item.py
@@ -1,8 +1,30 @@
 """Module to handle item processing"""
 # from mbu_rpa_core.exceptions import ProcessError, BusinessError
 
+from helpers import ats_functions, helper_functions
+
 
 def process_item(item_data: dict, item_reference: str):
     """Function to handle item processing"""
+
     assert item_data, "Item data is required"
     assert item_reference, "Item reference is required"
+
+    citizen_cpr = item_data.get("1805117229")
+
+    meta_data = {
+        "cpr": citizen_cpr,
+        "name": item_data.get("name")
+    }
+
+    process_name = "Frit valg"
+
+    helper_functions.handle_dashboard_run_creation(process_name=process_name, meta=meta_data)
+
+    helper_functions.handle_process_dashboard(status="success", item_reference=item_reference, process_step_name="Formular indsendt", process_name=process_name)
+
+    workqueue_name = "jou.solteqtand.tilflytter"
+
+    workqueue = ats_functions.fetch_workqueue(workqueue_name=workqueue_name)
+
+    ats_functions.enqueue_items(workqueue=workqueue, items=[item_data])
